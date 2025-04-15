d11c676ccd2c7f0e1bf54d2c758f317eaaefbac3
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2025-04-15 11:44:06 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,50 @@
 <script lang="ts" setup>
+import { onMounted } from "vue";
+import "@n8n/chat/style.css";
+import { createChat } from "@n8n/chat";
 
-import { onMounted } from 'vue';
-import '@n8n/chat/style.css';
-import { createChat } from '@n8n/chat';
-
-onMounted(() => {
-	createChat({
-		webhookUrl: 'https://autocosmos.app.n8n.cloud/webhook/your-webhook-id/chat',
+onMounted(async () => {
+  createChat({
+    webhookUrl: "https://n8n.2-end.com/webhook/your-webhook-id/chat",
     initialMessages: [
-		'Olá, me chamo João! Como posso ajudar?'
-	],
-  mode: "fullscreen",
-  showWelcomeScreen: false,
-  i18n: {
-		en: {
-			title: 'Promoções Farmarcas',
-			subtitle: "Aqui você encontra as principais informações sobre a Promoção Farmarcas!",
-			footer: '',
-			getStarted: '',
-			inputPlaceholder: 'Digite sua pergunta...',
-			closeButtonTooltip: 'Fechar chat'
-		},
-	},
-	})
-  
+      `
+### 💊 Olá! Seja bem-vindo(a)!
+
+Sou o consultor inteligente de precificação da Farmarcas, pronto para te ajudar com decisões estratégicas no varejo farmacêutico.
+Tenho acesso aos documentos e aprendizados registrados durante a consultoria, e posso analisar dados, responder dúvidas e sugerir melhorias com base em:
+
+- 📌 Estratégias de preço
+- 📊 Análise de mercado e concorrência
+- 🧠 Comportamento do consumidor
+- 📦 Sortimento, categorias e margem de contribuição
+- 📉 Elasticidade, markup, curva ABC e muito mais!
+
+Se precisar de alguma recomendação, insight ou análise, é só perguntar.
+Ah! Se eu não tiver informação suficiente, vou te avisar o que está faltando, beleza? 😉
+
+Qual sua dúvida de hoje? 💬`,
+      `🔄 Carregando... Aguarde um instante.`,
+    ],
+    mode: "fullscreen",
+    showWelcomeScreen: false,
+    i18n: {
+      en: {
+        title: "💵 Agente de Precificação",
+        subtitle: "",
+        footer: "",
+        getStarted: "",
+        inputPlaceholder: "Digite sua pergunta...",
+        closeButtonTooltip: "Fechar chat",
+      },
+    },
+  });
 });
 </script>
+
 <template>
-	<div style="width: 100%; height: 100%;" id="#n8n-chat"></div>
+  <div class="my-8">
+    <div class="max-w-screen-md mx-auto rounded-md overflow-hidden">
+      <div id="n8n-chat"></div>
+    </div>
+  </div>
 </template>
